1ffa9a033611ea356e936b8eba979cd62a660069
Alefy00 <alefy_sx@hotmail.com>
2025-06-23 18:12:24 -0300

    ajuste vercel

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,6 @@ import App from './App.jsx'
 import { LanguageProvider } from './constants/languageContext.jsx'
 
 
-
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <LanguageProvider>
