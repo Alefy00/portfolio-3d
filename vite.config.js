01d8c3c641e4263bf1082752c384edb1b64448d9
Alefy00 <alefy_sx@hotmail.com>
2025-08-22 13:28:31 -0300

    fix: sitemap

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,27 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
-
+import Sitemap from 'vite-plugin-sitemap' // <- import default
 
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
+    Sitemap({
+      hostname: 'https://www.alefyxavier.com.br', // sem barra no final
+      routes: [
+        '/',
+        '/skills',
+        '/experience',
+        '/work',
+        '/contact',
+        '/hero',
+      ],
+      // opcional:
+      // changefreq: 'weekly',
+      // priority: 1,
+      // dest: 'public', // se quiser gerar em /public em vez de /dist
+    }),
   ],
 })
