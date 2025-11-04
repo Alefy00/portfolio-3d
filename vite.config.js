e0585dbb37ef07d58771e50c2e5fbd05ff129cc0
Alefy00 <alefy_sx@hotmail.com>
2025-11-03 21:32:00 -0300

    fix: correct typo in vite config

--- vite.config.js
+++ vite.config.js
@@ -2,26 +2,29 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
-import Sitemap from 'vite-plugin-sitemap' // <- import default
+import Sitemap from 'vite-plugin-sitemap'
 
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
     Sitemap({
-      hostname: 'https://www.alefyxavier.com.br', // sem barra no final
-      routes: [
-        '/',
-        '/skills',
-        '/experience',
-        '/work',
-        '/contact',
-        '/hero',
-      ],
-      // opcional:
-      // changefreq: 'weekly',
-      // priority: 1,
-      // dest: 'public', // se quiser gerar em /public em vez de /dist
+      hostname: 'https://www.alefyxavier.com.br',
+      routes: ['/', '/skills', '/experience', '/work', '/contact', '/hero'],
     }),
   ],
+  build: {
+    // Aumenta o limite do aviso (padrão é 500 kB)
+    chunkSizeWarningLimit: 1500,
+
+    // Ajuda a separar libs grandes em chunks próprios
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ['react', 'react-dom'],
+          gsap: ['gsap', 'gsap/ScrollTrigger'],
+        },
+      },
+    },
+  },
 })
